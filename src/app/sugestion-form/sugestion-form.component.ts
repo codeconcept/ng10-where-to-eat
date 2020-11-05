685a4616284e7d49b3df9f020c636012a240ea72
codeconcept <contact@codeconcept.fr>
2020-11-05 18:47:55 +0100

    Angular Material + simple form

---
+++ src/app/sugestion-form/sugestion-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sugestion-form',
+  templateUrl: './sugestion-form.component.html',
+  styleUrls: ['./sugestion-form.component.css']
+})
+export class SugestionFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

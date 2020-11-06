4e95c1f5a3d664ac98f2e6c87afe7e3678c78fe7
codeconcept <contact@codeconcept.fr>
2020-11-06 12:44:16 +0100

    Retrieving the id of voted restaurant

--- src/app/suggestion-list/suggestion-list.component.ts
+++ src/app/suggestion-list/suggestion-list.component.ts
@@ -8,17 +8,22 @@ import { RestaurantService } from '../services/restaurant.service';
 @Component({
   selector: 'app-suggestion-list',
   templateUrl: './suggestion-list.component.html',
-  styleUrls: ['./suggestion-list.component.css']
+  styleUrls: ['./suggestion-list.component.css'],
 })
 export class SuggestionListComponent implements OnInit {
   restaurantsCollection: AngularFirestoreCollection<Restaurant>;
   restaurants$: Observable<Restaurant[]>;
 
-  constructor(private rs: RestaurantService) { }
+  constructor(private rs: RestaurantService) {}
 
   async ngOnInit(): Promise<void> {
     this.restaurantsCollection = await this.rs.readRestaurants();
-    this.restaurants$ = this.restaurantsCollection.valueChanges();
+    this.restaurants$ = this.restaurantsCollection.valueChanges({
+      idField: 'id',
+    });
   }
 
+  vote(id) {
+    console.log(id);
+  }
 }
